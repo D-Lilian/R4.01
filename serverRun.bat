59f71a62f4006e2d1bbadf5336bb67b1a8d9b3f0
D-Lilian <lilian.daurat@gmail.com>
2024-03-19 17:06:20 +0100

    Update BATman

--- serverRun.bat
+++ serverRun.bat
@@ -4,4 +4,3 @@ php bin\console doctrine:schema:drop --full-database --force
 php bin\console doctrine:schema:create
 php bin\console doctrine:fixtures:load --append
 php -S 0.0.0.0:8000 -t public 
-bin/console dbal:run-sql "INSERT INTO admin (id, username, roles, password) values (0, 'root', '[\"ROLE_ADMIN\"]', '\$2y\$13\$W4BNB29zUfRR.W0WXPa3EeNQ04T/gDIHJL/LCBlf4O4orbL.JN106');"
